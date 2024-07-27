d12d83a9f02e420da6225bae160c82d947206669
Rohan Allison <rohanthewiz_at_gmail.com>
2024-07-27 13:56:06 -0500

    Add some styling to our page and table

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 )
 
 func main() {
+	// Create a new web server
 	app := fiber.New()
 
 	// Define routes
@@ -15,32 +16,44 @@ func main() {
 		return c.SendString("Welcome to the Fiber web server!")
 	})
 
-	app.Get("/hello/:name", func(c *fiber.Ctx) error {
+	app.Get("/sayhello/:name", func(c *fiber.Ctx) error {
 		name := c.Params("name")
 		return c.SendString("Hello, " + name + "!")
 	})
 
 	// New route to serve the HTML form
-	app.Get("/form", func(c *fiber.Ctx) error {
-
-		text := `<form action="/submit" method="post">
+	app.Get("/form",
+		func(c *fiber.Ctx) error {
+			text := `
+			<body style="background-color: slategrey;font-weight: bold;">
+			<form action="/submit" method="post">
 				<label for="name">Name:</label>
 				<input type="text" id="name" name="name" required><br><br>
+
 				<label for="age">Age:</label>
 				<input type="number" id="age" name="age" required><br><br>
+
 				<input type="submit" value="Submit">
-			</form>`
-		html(c, text, true)
-		html(c, "<p>The End</p>", false)
-		return nil
-	})
+			</form>
+			</body>`
+			html(c, text, true)
+			html(c, "<p>The End</p>", false)
+			return nil
+		},
+	)
 
 	// New route to handle form submission
 	app.Post("/submit", func(c *fiber.Ctx) error {
 		name := c.FormValue("name")
 		age := c.FormValue("age")
 
-		resp := fmt.Sprintf(`<table border=1><tr><td style="color:#f0e020">Name: %s</td><td> Age: %s</td></tr></table>`, name, age)
+		resp := fmt.Sprintf(`
+			<body style="background-color: slategrey;font-weight: bold;">
+			<table border=1 cellpadding=3>
+				<tr><td style="color:#3adeda">Name: %s</td><td> Age: %s</td></tr>
+			</table>
+			</body>`, name, age)
+
 		html(c, resp, true)
 		return nil
 	})
